b5d212095794b1657a2480ee94cb814076bfc70c
minsuk.jang <minsuk.jang@influential.co.kr>
2024-01-11 12:29:26 +0900

    테스트 환경

--- app/src/main/java/com/example/lifecycletest/MainActivity.kt
+++ app/src/main/java/com/example/lifecycletest/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.example.lifecycletest
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.lifecycletest.databinding.ActivityMainBinding
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlin.math.log
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
@@ -24,14 +17,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        /*supportFragmentManager.beginTransaction().apply {
-            add(binding.frameLayout.id, FirstFragment.getInstance())
-        }.commit()*/
-
         repeatOnLifecycleStarted()
         launchWhenStarted()
     }
 
+
+    //TODO: 시스템에서 값이 나오는 형태로
     private fun repeatOnLifecycleStarted() {
         lifecycleScope.launch {
             repeatOnLifecycle(state = Lifecycle.State.STARTED) {
--- app/src/main/java/com/example/lifecycletest/MainActivityViewModel.kt
+++ app/src/main/java/com/example/lifecycletest/MainActivityViewModel.kt
@@ -1,15 +1,9 @@
 package com.example.lifecycletest
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 
 class MainActivityViewModel : ViewModel() {
     private val _state: Flow<Int> = flow {
